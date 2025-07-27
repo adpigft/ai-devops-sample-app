f1b0ad53a26f20033fd596549be9f73a479be22e
johndoe <johndoe@email.com>
2025-07-27 16:52:12 +0800

    Initial commit

---
+++ src/main/java/com/example/AiDevOpsApp.java
@@ -0,0 +1,11 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiDevOpsApp {
+    public static void main(String[] args) {
+        SpringApplication.run(AiDevOpsApp.class, args);
+    }
+}
